91f539a8e7d06192423b3fcfb535a50695759229
zhiyan <1533460130@qq.com>
2022-08-28 12:46:52 +0800

    feat: 添加spring boot 依赖

---
+++ src/main/java/com/ciusyan/SpringConstructApplication.java
@@ -0,0 +1,13 @@
+package com.ciusyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConstructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConstructApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ciusyan/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.ciusyan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tests")
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!!!";
+    }
+}
